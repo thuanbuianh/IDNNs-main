3b226ada5d7ce6fa6a7c1731862397c6e4245ea6
thuanbuianh <19521001@gm.uit.edu.vn>
2022-04-21 18:34:37 +0700

    demo calculate MMI

--- idnns/information/mutual_information_calculation.py
+++ idnns/information/mutual_information_calculation.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import multiprocessing
+from sklearn.metrics.pairwise import rbf_kernel
 from joblib import Parallel, delayed
 
 NUM_CORES = multiprocessing.cpu_count()
@@ -36,13 +37,44 @@ def calc_condtion_entropy(px, t_data, unique_inverse_x):
 	return H_TgX
 
 
-def calc_information_from_mat(px, py, ps2, data, unique_inverse_x, unique_inverse_y, unique_array):
+def calc_information_from_mat(px, py, ps2, data, unique_inverse_x, unique_inverse_y, unique_array, x, ws_layer):
 	"""Calculate the MI based on binning of the data"""
-	H2 = -np.sum(ps2 * np.log2(ps2))	# H(T)
-	H2X = calc_condtion_entropy(px, data, unique_inverse_x)	# H(T|X)
-	H2Y = calc_condtion_entropy(py.T, data, unique_inverse_y) # H(T|Y)
-	IY = H2 - H2Y	# I(T,X) == I(X,T) 
-	IX = H2 - H2X	# I(T,Y) == I(Y,T)
+	# H2 = -np.sum(ps2 * np.log2(ps2))	# H(T)
+	# H2X = calc_condtion_entropy(px, data, unique_inverse_x)	# H(T|X)
+	# H2Y = calc_condtion_entropy(py.T, data, unique_inverse_y) # H(T|Y)
+	# IY = H2 - H2Y	# I(T,X) == I(X,T) 
+	# IX = H2 - H2X	# I(T,Y) == I(Y,T)
+
+	# Tính MMI dựa theo công thức (7)
+	B = np.zeros(x.shape[0])
+	As = [np.zeros(x.shape[0]) for i in range(ws_layer[-1])]
+	for i in range(x.shape[0]):
+		for j in range(x.shape[0]):
+			B[i][j] = rbf_kernel(x[i], x[j])
+			for k in range(len(As)):
+				As[k][i][j] = rbf_kernel(ws_layer[i][:][:][k], ws_layer[j][:][:][k])
+	# Viết lại công thức (7): I(B; {A1; A2; · · · ; AC}) = S(B) + S(As_scaled) - S(AsB_scaled)
+	As_temp = As[0]
+	for i in range(1, len(As)):
+		As_temp = np.multiply(As_temp, As[i])	
+	As_scaled = As_temp / np.matrix.trace(As_temp)
+
+	AsB_temp = np.multiply(As_temp, B)
+	AsB_scaled = AsB_temp / np.matrix.trace(AsB_temp)
+
+	# Tính các đại lượng S_alpha theo công thức (2)
+	alpha = 1.01
+	B_ev = np.linalg.eig(B) # B's eigenvalues 
+	S_B = np.log2(np.sum(B_ev**alpha)) / (1-alpha)
+
+	As_scaled_ev = np.linalg.eig(As_scaled)
+	S_As_scaled = np.log2(np.sum(As_scaled_ev**alpha)) / (1-alpha)
+
+	AsB_scaled_ev = np.linalg.eig(AsB_scaled)
+	S_AsB_scaled = np.log2(np.sum(AsB_scaled_ev**alpha)) / (1-alpha)
+
+	IX = S_B + S_As_scaled - S_AsB_scaled
+	IY = IX
 	return IX, IY
 
 
